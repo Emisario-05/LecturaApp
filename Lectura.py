00f3aca34976c5fca862fbd084d83653d7c5fc93
nattpena <america.salcedo.1792@alumnos.udg.mx>
2024-08-27 17:50:21 -0600

    P4 Url_for

--- Lectura.py
+++ Lectura.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 RpgApp = Flask(__name__)
 
 @RpgApp.route('/')
 
 def home():
-    return '<h1> Hola pendejos </h1>'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     RpgApp.run(debug=True,port=3300)
